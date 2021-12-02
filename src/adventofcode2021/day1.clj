2a952c991466ce9a01f13deb06e9c2bac1f135b2
nawhi <25394965+nawhi@users.noreply.github.com>
2021-12-02 10:12:56 +0000

    Day 1 puzzle 1

--- src/adventofcode2021/day1.clj
+++ src/adventofcode2021/day1.clj
@@ -1,7 +1,9 @@
 (ns adventofcode2021.day1)
 
+(defn- is-increase [[m1 m2]] (if (< m1 m2) 1 0))
+
 (defn count-measurement-increase
   "Count the number of times a measurement increases from the previous"
   [measurements]
-  (let [[m1 m2] measurements]
-    (if (< m1 m2) 1 0)))
+  (reduce #(+ %1 (is-increase %2)) 0 (partition 2 1 measurements)))
+
--- test/adventofcode2021/day1_test.clj
+++ test/adventofcode2021/day1_test.clj
@@ -4,4 +4,15 @@
 
 (deftest a-test
   (testing "How many measurements are larger than the previous"
-    (is (= 1 (count-measurement-increase [1 2])))))
+    (is (= 1 (count-measurement-increase [1 2])))
+    (is (= 5 (count-measurement-increase [1 2 3 4 6 8])))
+    (is (= 3 (count-measurement-increase [10 15 10 15 10 15])))
+    (is (= 2 (count-measurement-increase [1 2 3])))))
+
+(deftest puzzle1
+  (testing "puzzle 1"
+    (let [raw-input (slurp "resources/1.1.txt")
+          split-input (clojure.string/split raw-input #"\n" )
+          numeric-input (map #(Integer/parseInt %) split-input)
+          result (count-measurement-increase numeric-input)]
+      (is (= 1226 result)))))
