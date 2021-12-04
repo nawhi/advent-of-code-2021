43b073530704d4dc9d69cdf48770c544fb558af4
nawhi <25394965+nawhi@users.noreply.github.com>
2021-12-04 22:16:31 +0000

    day 4 puzzle 1: move into separate source file

--- test/adventofcode2021/day4_test.clj
+++ test/adventofcode2021/day4_test.clj
@@ -1,15 +1,6 @@
 (ns adventofcode2021.day4-test
-  (:require [clojure.test :refer :all]))
-
-(defn- row-has-won [board]
-  (some #(every? (fn [[_ drawn]] drawn) %) board))
-
-(defn- transpose [m] (apply mapv vector m))
-
-(defn has-won [board]
-  (let [board-2d (to-array-2d board)]
-    (boolean (or (row-has-won board-2d)
-                 (row-has-won (transpose board-2d))))))
+  (:require [clojure.test :refer :all]
+            [adventofcode2021.day4 :refer :all]))
 
 (deftest has-board-won
   (testing "1 entry"
