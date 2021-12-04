0455b1dc3868b07b3dbfbcac090f57a678d72300
nawhi <25394965+nawhi@users.noreply.github.com>
2021-12-04 22:03:05 +0000

    day 4 puzzle 1: finish row-has-won using mapv magic

--- test/adventofcode2021/day4_test.clj
+++ test/adventofcode2021/day4_test.clj
@@ -1,26 +1,33 @@
 (ns adventofcode2021.day4-test
   (:require [clojure.test :refer :all]))
 
-(defn row-has-won [row]
-  (every? (fn [[_ drawn]] drawn) row))
+(defn all-drawn [row] (every? (fn [[_ drawn]] drawn) row))
+
+(defn- row-has-won [board]
+  (some #(all-drawn %) board))
+
+(defn- col-has-won [board]
+  (let [row (mapv first board)]
+    (all-drawn row)))
 
 (defn has-won [board]
-  (boolean (some #(row-has-won %) board)))
+  (let [board-2d (to-array-2d board)]
+    (boolean (or (row-has-won board-2d)
+                 (col-has-won board-2d)))))
 
 (deftest has-board-won
   (testing "1 entry"
     (is (= false (has-won [[[1 false]]])))
     (is (= true (has-won [[[1 true]]])))
     )
-  (testing "2 entries 1 row"
-    (is (= true (has-won [[[1 true] [2 true]]])))
-    (is (= false (has-won [[[1 true] [2 false]]])))
-    )
   (testing "diagonal is not a win"
     (is (= false (has-won [[[1 true] [2 false]]
                            [[3 false] [4 true]]])))
     )
-  ;(testing "second row can win"
-  ;  (is (= true (has-won [[[1 true] [2 false]]
-  ;                        [[3 true] [4 true]]]))))
+  (testing "second row can win"
+    (is (= true (has-won [[[1 false] [2 false]]
+                          [[3 true] [4 true]]]))))
+  (testing "column can win"
+    (is (= true (has-won [[[1 true] [2 false]]
+                          [[3 true] [4 false]]]))))
   )
