cb14a4cb24133fec20ba05f5234c4be541ba38dd
nawhi <25394965+nawhi@users.noreply.github.com>
2021-12-04 20:52:05 +0000

    Day 3 puzzle 1: epsilon and gamma

--- src/adventofcode2021/day3.clj
+++ src/adventofcode2021/day3.clj
@@ -7,12 +7,24 @@
         zeroes (get group false)]
     (if (> (count ones) (count zeroes)) "1" "0")))
 
-(defn epsilon
+(defn most-common-bit [digits]
+  (let [group (group-by #(= "1" %) digits)
+        ones (get group true)
+        zeroes (get group false)]
+    (if (> (count ones) (count zeroes)) "0" "1")))
+
+
+(defn- decode
   "Returns number with the least common bit at each position for each input number"
-  [numbers]
+  [reducer numbers]
   (let [num-length (count (first numbers))]
     (loop [i 0 res ""]
       (if (= i num-length)
         (Integer/parseInt res 2)
-        (let [bit-this-pos (least-common-bit (map #(subs % i (+ i 1)) numbers))]
+        (let [bit-this-pos (reducer (map #(subs % i (+ i 1)) numbers))]
           (recur (+ i 1) (str res bit-this-pos)))))))
+
+(def epsilon (partial decode least-common-bit))
+(def gamma (partial decode most-common-bit))
+
+
