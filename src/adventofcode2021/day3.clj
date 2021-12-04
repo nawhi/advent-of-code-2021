9e53f713a06f6d16ee9b0619208db70094e1c684
nawhi <25394965+nawhi@users.noreply.github.com>
2021-12-04 16:58:11 +0000

    Day 3 puzzle 1: refactor

--- src/adventofcode2021/day3.clj
+++ src/adventofcode2021/day3.clj
@@ -1,11 +1,12 @@
 (ns adventofcode2021.day3)
 
-
-
-(defn epsilon
-  "Returns number with the least common bit at each position for each input number"
-  [digits]
+(defn least-common-bit [digits]
   (let [group (group-by #(= "1" %) digits)
         ones (get group true)
         zeroes (get group false)]
     (if (> (count ones) (count zeroes)) "1" "0")))
+
+(defn epsilon
+  "Returns number with the least common bit at each position for each input number"
+  [numbers]
+  (least-common-bit numbers))
