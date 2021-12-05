2abd99aa47a76f54992f64c1c8c3a9a0c23decf0
nawhi <25394965+nawhi@users.noreply.github.com>
2021-12-05 12:06:15 +0000

    day 4 puzzle 1: calculate score

--- src/adventofcode2021/day4.clj
+++ src/adventofcode2021/day4.clj
@@ -3,10 +3,18 @@
 (defn- transpose [m] (apply mapv vector m))
 
 (defn- row-win? [board draws]
-  (let [[row] board]
-    (every? #(contains? draws %) row)))
+  (some (fn [row]
+          (every? #(contains? draws %) row)) board))
 
-(defn win? [board draws]
+(defn- win? [board draws]
   (or (row-win? board draws) (row-win? (transpose board) draws)))
 
-  (defn score [board draws] (if (win? board (set draws)) 0 nil))
+(defn- calc-score [board draws last-drawn]
+  (let [undrawn (filter #(not (contains? draws %)) (flatten board))]
+    (* (reduce + undrawn) last-drawn)))
+
+(defn score [board draws]
+  (let [draws-set (set draws)]
+    (if (win? board draws-set)
+      (calc-score board draws-set (last draws))
+      nil)))
