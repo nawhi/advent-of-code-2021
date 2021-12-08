38b8c04f4d31afd71315ef630222909c50ecc44b
nawhi <25394965+nawhi@users.noreply.github.com>
2021-12-08 18:29:08 +0000

    day 6 puzzle 1: add state matrix

--- test/adventofcode2021/day6_test.clj
+++ test/adventofcode2021/day6_test.clj
@@ -1,14 +1,28 @@
 (ns adventofcode2021.day6-test
   (:require [clojure.test :refer :all]))
 
-(defn count-fish [[starting-fish] num-days]
-  (if (> num-days starting-fish) 2 1))
+(def STATE-TRANSITION-MX
+  "foo"
+  [[0 0 0 0 0 0 0 0 1]
+   [1 0 0 0 0 0 0 0 0]
+   [0 1 0 0 0 0 0 0 1]
+   [0 0 1 0 0 0 0 0 0]
+   [0 0 0 1 0 0 0 0 0]
+   [0 0 0 0 1 0 0 0 0]
+   [0 0 0 0 0 1 0 0 0]
+   [0 0 0 0 0 0 1 0 0]
+   [0 0 0 0 0 0 0 1 0]])
+
+(defn count-fish [[age] iterations]
+  (if (>= (- age iterations) 0)
+    1
+    (let [offset (mod age 6)])
+    ))
 
 (deftest count-fish-test
-  (testing "single fish lasts days minus age"
-    (is (= 1 (count-fish [1] 1)))
-    (is (= 1 (count-fish [5] 5)))
-    (is (= 1 (count-fish [1] 1))))
-  (testing "fish spawns another fish when its age reaches zero"
-    (is (= 1 (count-fish [1] 1)))
-    (is (= 2 (count-fish [1] 2)))))
+  (are [expected fish iterations]
+    (= expected (count-fish fish iterations))
+    1 [1] 1
+    1 [6] 6
+    2 [0] 1
+    2 [6] 7))
