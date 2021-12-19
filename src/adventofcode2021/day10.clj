61ed3d332dcadd89fcb6b37479bc833967282077
nawhi <25394965+nawhi@users.noreply.github.com>
2021-12-19 19:23:35 +0000

    day 10 puzzle 1: complete

--- src/adventofcode2021/day10.clj
+++ src/adventofcode2021/day10.clj
@@ -5,15 +5,22 @@
 (def match-paren (partial get {\[ \] \( \) \{ \} \< \>}))
 (def score (partial get {\) 3 \] 57 \} 1197 \> 25137}))
 
-(defn parse-internal [line stack]
-  (println "parse-internal '" line "', stack=" stack)
-  (if (empty? line) nil
-  (let [[c] line expected (last stack)]
-    (if (is-close-paren c)
-      (if (= expected c)
-        (parse-internal (subs line 1) (pop stack))
-        c)
-      (parse-internal (subs line 1) (conj stack (match-paren c)))))))
+(defn- parse-internal [line stack]
+  (if (empty? line)
+    nil
+    (let [[c] line expected (last stack)]
+      (if (is-close-paren c)
+        (if (= expected c)
+          (parse-internal (subs line 1) (pop stack))
+          c)
+        (parse-internal (subs line 1) (conj stack (match-paren c)))))))
 
 
 (defn parse [line] (parse-internal line []))
+
+(defn syntax-score [lines]
+  (->> lines
+       (map #(parse %))
+       (map score)
+       (filter some?)
+       (reduce +)))
--- test/adventofcode2021/day10_test.clj
+++ test/adventofcode2021/day10_test.clj
@@ -1,8 +1,9 @@
 (ns adventofcode2021.day10-test
   (:require [clojure.test :refer :all]
+            [adventofcode2021.utils :refer :all]
             [adventofcode2021.day10 :refer :all]))
 
-(deftest syntax-score-test
+(deftest parse-test
   (are [string expected]
     (= (parse string) expected)
     ")" \)
@@ -29,3 +30,20 @@
     "[<(<(<(<{}))><([]([]()" \)
     "<{([([[(<>()){}]>(<<{{" \>
     ))
+
+(deftest syntax-score-test
+  (testing "example from puzzle description"
+    (let [lines ["[({(<(())[]>[[{[]{<()<>>"
+                 "[(()[<>])]({[<{<<[]>>("
+                 "{([(<{}[<>[]}>{[]{[(<()>"
+                 "(((({<>}<{<{<>}{[]{[]{}"
+                 "[[<[([]))<([[{}[[()]]]"
+                 "[{[{({}]{}}([{[{{{}}([]"
+                 "{<[[]]>}<{[{[{[]{()[[[]"
+                 "[<(<(<(<{}))><([]([]()"
+                 "<{([([[(<>()){}]>(<<{{"
+                 "<{([{{}}[<[[[<>{}]]]>[]]"]]
+      (is (= 26397 (syntax-score lines)))))
+  (testing "puzzle 1"
+    (is (= 318099 (syntax-score (puzzle-input "10.1")))))
+  )
