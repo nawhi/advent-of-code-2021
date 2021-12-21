0019f79fd4cab3989564d98c66e161bf61fc9492
nawhi <25394965+nawhi@users.noreply.github.com>
2021-12-21 19:16:34 +0000

    day 12: puzzle 1!!

--- src/adventofcode2021/day12.clj
+++ src/adventofcode2021/day12.clj
@@ -3,16 +3,37 @@
 
 (defn parse-cave [lines]
   (reduce (fn [caves line]
-            (let [[from to] (string/split line #"-")]
-              (update caves from #(set (conj % to)))))
+            (let [[node1 node2] (string/split line #"-")]
+              (-> caves
+                  (update node1 #(set (conj % node2)))
+                  (update node2 #(set (conj % node1))))))
           {}
           lines))
 
+(defn big-cave?
+  "Big caves are all upper case and can be visited more than once"
+  [node]
+  (= (string/upper-case node) node))
+
+(defn debug [x] (println "[debug]" x) x)
+
+(defn already-visited [route node]
+  (boolean (some #{node} route)))
+
+(defn visitable-children
+  "A child is visitable if it is either a big cave or has not been visited before"
+  [graph node route]
+  (->> (get graph node)
+       (filter #(or (big-cave? %) (not (already-visited route %))))))
+
 (defn routes- [graph node route]
   (if (= node "end")
-    route
-    (for [child (get graph node)]
-      (routes- graph child (conj route child)))))
+    (string/join "," route)
+    (let [children (visitable-children graph node route)]
+      (for [child children] (routes- graph child (conj route child)))
+      )))
 
-(defn routes [graph]
-  (set (routes- graph "start" ["start"])))
+(defn routes [lines]
+  (let [cave (parse-cave lines)]
+    (println cave)
+    (set (flatten (routes- cave "start" ["start"])))))
--- test/adventofcode2021/day12_test.clj
+++ test/adventofcode2021/day12_test.clj
@@ -3,28 +3,37 @@
             [adventofcode2021.day12 :refer :all]
             [adventofcode2021.utils :refer [puzzle-input]]))
 
+
 (deftest day12-test
   (testing "parse cave system from string"
     (are [input expected]
       (is (= (parse-cave input) expected))
-      ["a-b"] {"a" #{"b"}}
-      ["a-b" "a-c"] {"a" #{"b" "c"}}
-      ["a-b" "a-c" "a-d"] {"a" #{"b" "c" "d"}}
-      ["a-b" "a-c" "a-d" "b-d" "b-e"] {"a" #{"b" "c" "d"} "b" #{"d" "e"}}
-      (puzzle-input "12.1") {"DG"    #{"dc"}
-                             "MN"    #{"ah" "dc" "fi" "start" "wn"}
-                             "XB"    #{"fi"}
-                             "dc"    #{"XB" "ah"}
-                             "end"   #{"XB" "yw"}
-                             "fi"    #{"end" "yw"}
-                             "start" #{"ah" "dc"}
-                             "th"    #{"fi"}
-                             "wn"    #{"DG" "XB" "ah" "yw"}
-                             "yw"    #{"DG" "MN" "XN"}}
+      ["a-b"] {"a" #{"b"} "b" #{"a"}}
+      ["a-b" "a-c"] {"a" #{"b" "c"} "b" #{"a"} "c" #{"a"}}
+      ["a-b" "a-c" "a-d"] {"a" #{"b" "c" "d"} "b" #{"a"} "c" #{"a"} "d" #{"a"}}
+      ["a-b" "a-c" "a-d" "b-d" "b-e"] {"a" #{"b" "c" "d"}
+                                       "b" #{"a" "d" "e"}
+                                       "c" #{"a"}
+                                       "d" #{"a" "b"}
+                                       "e" #{"b"}}
+      ["start-x" "x-end"] {"start" #{"x"} "x" #{"start" "end"} "end" #{"x"}}
       ))
   (testing "find all routes through a cave system"
-    (are [graph expected]
-      (is (= (routes graph) expected))
-      {"start" #{"end"}} #{["start" "end"]}
+    (are [input expected]
+      (is (= (routes input) expected))
+      ["start-end"] #{"start,end"}
+      ["start-x" "x-end"] #{"start,x,end"}
+      ["start-x" "x-y" "y-end"] #{"start,x,y,end"}
+      ["start-x" "start-y" "x-end" "y-end"] #{"start,x,end" "start,y,end"}
       ))
+  (testing "larger examples"
+    (let [example1 ["start-A" "start-b" "A-c" "A-b" "b-d" "A-end" "b-end"]
+          example2 ["dc-end" "HN-start" "start-kj" "dc-start" "dc-HN" "LN-dc" "HN-end" "kj-sa" "kj-HN" "kj-dc"]
+          example3 ["fs-end" "he-DX" "fs-he" "start-DX" "pj-DX" "end-zg" "zg-sl" "zg-pj" "pj-he" "RW-he" "fs-DX" "pj-RW" "zg-RW" "start-pj" "he-WI" "zg-he" "pj-fs" "start-RW"]]
+      (is (= (count (routes example1)) 10))
+      (is (= (count (routes example2)) 19))
+      (is (= (count (routes example3)) 226))
+      ))
+  (testing "puzzle 1"
+    (is (= (count (routes (puzzle-input "12.1"))) 12345)))
   )
