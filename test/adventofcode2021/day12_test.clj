1076020459542dcf45a1485669e328d2fe2b6cff
nawhi <25394965+nawhi@users.noreply.github.com>
2021-12-21 13:56:29 +0000

    day 12: add puzzle input & cave parser test

--- test/adventofcode2021/day12_test.clj
+++ test/adventofcode2021/day12_test.clj
@@ -1,6 +1,7 @@
 (ns adventofcode2021.day12-test
   (:require [clojure.test :refer :all]
-            [adventofcode2021.day12 :refer :all]))
+            [adventofcode2021.day12 :refer :all]
+            [adventofcode2021.utils :refer [puzzle-input]]))
 
 (deftest day12-test
   (testing "parse cave system from string"
@@ -10,4 +11,14 @@
       ["a-b" "a-c"] {"a" #{"b" "c"}}
       ["a-b" "a-c" "a-d"] {"a" #{"b" "c" "d"}}
       ["a-b" "a-c" "a-d" "b-d" "b-e"] {"a" #{"b" "c" "d"} "b" #{"d" "e"}}
+      (puzzle-input "12.1") {"DG"    #{"dc"}
+                             "MN"    #{"ah" "dc" "fi" "start" "wn"}
+                             "XB"    #{"fi"}
+                             "dc"    #{"XB" "ah"}
+                             "end"   #{"XB" "yw"}
+                             "fi"    #{"end" "yw"}
+                             "start" #{"ah" "dc"}
+                             "th"    #{"fi"}
+                             "wn"    #{"DG" "XB" "ah" "yw"}
+                             "yw"    #{"DG" "MN" "XN"}}
       )))
