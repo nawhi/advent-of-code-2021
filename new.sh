cf6387cbcfc05d357d9624a9afe6fee5324aef93
nawhi <25394965+nawhi@users.noreply.github.com>
2021-12-22 14:42:27 +0000

    Add untested script for creating new puzzle template

---
+++ new.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+day_num=${1?:pass a day number as first argument}
+cat > test/adventofcode2021/day${day_num}_test.clj << _HERE_
+(ns adventofcode2021.day13-test
+  (:require [clojure.test :refer :all]
+            [adventofcode2021.day${day_num} :refer :all]
+            [adventofcode2021.utils :refer [puzzle-input]]))
+
+(deftest day${day_num}_test
+  (testing "puzzle 1"
+      (are [x expected]
+        (is (= (my-func x) expected))
+        "abc" nil
+        )))
+_HERE_
+
+cat > src/adventofcode2021/day${day_num}.clj << _HERE_
+(ns adventofcode2021.day${day_num})
+
+(defn my-func [_] nil)
+_HERE_
