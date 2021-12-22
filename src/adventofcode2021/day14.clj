c4f15eb173e972ebc98eb70808563f579f316170
nawhi <25394965+nawhi@users.noreply.github.com>
2021-12-22 15:09:05 +0000

    day 14: puzzle 1

--- src/adventofcode2021/day14.clj
+++ src/adventofcode2021/day14.clj
@@ -1,3 +1,17 @@
-(ns adventofcode2021.day14)
+(ns adventofcode2021.day14
+  (:require [clojure.string :as string]))
 
-(defn my-func [_] nil)
+(defn- parse-rules [rules]
+  (->> rules
+       (map #(string/split % #" -> "))
+       (into {})))
+
+(defn- handle-pair [[_ second :as pair] rules]
+  (str (get rules (apply str pair)) second))
+
+(defn simulate-pair-insertion [template rules]
+  (let [rules (parse-rules rules)]
+    (str (first template) (->> template
+                               (partition 2 1)
+                               (map #(handle-pair % rules))
+                               (#(string/join "" %))))))
--- test/adventofcode2021/day14_test.clj
+++ test/adventofcode2021/day14_test.clj
@@ -3,9 +3,20 @@
             [adventofcode2021.day14 :refer :all]
             [adventofcode2021.utils :refer [puzzle-input]]))
 
+(def EXAMPLE-RULES ["CH -> B" "HH -> N" "CB -> H" "NH -> C"
+                    "HB -> C" "HC -> B" "HN -> C" "NN -> C"
+                    "BH -> H" "NC -> B" "NB -> B" "BN -> B"
+                    "BB -> N" "BC -> B" "CC -> N" "CN -> C"])
+
 (deftest day14_test
   (testing "puzzle 1"
-    (are [x expected]
-      (is (= (my-func x) expected))
-      "abc" nil
+    (are [template rules expected]
+      (is (= (simulate-pair-insertion template rules) expected))
+      "AB" ["CD -> E"] "AB"
+      "AB" ["AB -> E"] "AEB"
+      "ABC" ["AB -> E" "BC -> F"] "AEBFC"
+      "NNCB" EXAMPLE-RULES "NCNBCHB"
+      "NCNBCHB" EXAMPLE-RULES "NBCCNBBBCBHCB"
+      "NBCCNBBBCBHCB" EXAMPLE-RULES "NBBBCNCCNBBNBNBBCHBHHBCHB"
+      "NBBBCNCCNBBNBNBBCHBHHBCHB" EXAMPLE-RULES "NBBNBNBBCCNBCNCCNBBNBBNBBBNBBNBBCBHCBHHNHCBBCBHCB"
       )))
